e8c843b65628d2c7c00ddf6af93a3a525713c96d
UncleHandsome <yunwei@multicorewareinc.com>
2013-11-02 00:48:48 +0800

    1. ALU_Ctrl to use FUNC parameter to indicate the instruction 2. Decoder add new flag MemToReg and MemWrite, and update to use parameter op 3. Reg_File initialize r29(stack pointer) to 128

--- ALU_Ctrl.v
+++ ALU_Ctrl.v
@@ -25,6 +25,17 @@ output     [4-1:0] ALUCtrl_o;
 reg        [4-1:0] ALUCtrl_o;
 
 //Parameter
+parameter [6-1:0] FUNC_ADD = 6'b100000;
+parameter [6-1:0] FUNC_SUB = 6'b100010;
+parameter [6-1:0] FUNC_AND = 6'b100100;
+parameter [6-1:0] FUNC_OR  = 6'b100101;
+parameter [6-1:0] FUNC_SLT = 6'b101010;
+parameter [6-1:0] FUNC_SLLV= 6'b000100;
+parameter [6-1:0] FUNC_SLL = 6'b000000;
+parameter [6-1:0] FUNC_SRLV= 6'b000110;
+parameter [6-1:0] FUNC_SRL = 6'b000010;
+parameter [6-1:0] FUNC_MUL = 6'b011000;
+parameter [6-1:0] FUNC_JR  = 6'b001000;
 
        
 //Select exact operation
@@ -32,18 +43,18 @@ always @ (*) begin
     case (ALUOp_i)
         3'b000: ALUCtrl_o <= 4'b0010; // add (for addi)
         3'b001: ALUCtrl_o <= 4'b0110; // sub (for beq)
-        3'b010: ALUCtrl_o <= 4'b0001; // add (for addi)
+        3'b010: ALUCtrl_o <= 4'b0001; // or (for ori)
         default: case(funct_i) // R-type instruction  
-            6'b100000: ALUCtrl_o <= 4'b0010; // add
-            6'b100010: ALUCtrl_o <= 4'b0110; // sub
-            6'b100100: ALUCtrl_o <= 4'b0000; // and
-            6'b100101: ALUCtrl_o <= 4'b0001; // or
-            6'b101010: ALUCtrl_o <= 4'b0111; // slt
-            6'b000000: ALUCtrl_o <= 4'b1000; // sll
-            6'b000100: ALUCtrl_o <= 4'b1010; // sllv
-            6'b000010: ALUCtrl_o <= 4'b1001; // srl
-            6'b000110: ALUCtrl_o <= 4'b1011; // srlv
-            default:   ALUCtrl_o <= 4'bxxxx; // ???
+            FUNC_ADD : ALUCtrl_o <= 4'b0010;
+            FUNC_SUB : ALUCtrl_o <= 4'b0110;
+            FUNC_ADD : ALUCtrl_o <= 4'b0000;
+            FUNC_OR  : ALUCtrl_o <= 4'b0001;
+            FUNC_SLT : ALUCtrl_o <= 4'b0111;
+            FUNC_SLL : ALUCtrl_o <= 4'b1000;
+            FUNC_SLLV: ALUCtrl_o <= 4'b1010;
+            FUNC_SRL : ALUCtrl_o <= 4'b1001;
+            FUNC_SRLV: ALUCtrl_o <= 4'b1011;
+            default:   ALUCtrl_o <= 4'bxxxx;
         endcase
     endcase
 end
--- Decoder.v
+++ Decoder.v
@@ -16,7 +16,9 @@ module Decoder(
 	ALUSrc_o,
 	RegDst_o,
 	Branch_o,
-    SinExt_o
+    SinExt_o,
+    MemToReg_o,
+    MemWrite_o
 	);
 
 //I/O ports
@@ -28,25 +30,37 @@ output         ALUSrc_o;
 output         RegDst_o;
 output         Branch_o;
 output         SinExt_o;
+output         MemToReg_o;
+output         MemWrite_o;
 
-//Internal Signals
-reg    [3-1:0] ALU_op_o;
-reg            ALUSrc_o;
-reg            RegWrite_o;
-reg            RegDst_o;
-reg            Branch_o;
-reg            SinExt_o;
+reg [10:0]   countrol;
+assign {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, MemWrite_o, MemToReg_o, ALU_op_o, SinExt_o} = control;
 
 //Parameter
+parameter [6-1:0] OP_RTYPE = 6'b000000;
+parameter [6-1:0] OP_ADDI  = 6'b001000;
+parameter [6-1:0] OP_BEQ   = 6'b000100;
+parameter [6-1:0] OP_ORI   = 6'b001101;
+parameter [6-1:0] OP_LW	   = 6'b100011;
+parameter [6-1:0] OP_SW    = 6'b101011;
+parameter [6-1:0] OP_JUMP  = 6'b000010;
+parameter [6-1:0] OP_BGT   = 6'b000111;
+parameter [6-1:0] OP_BNEZ  = 6'b000101;
+parameter [6-1:0] OP_BGEZ  = 6'b000001;
+parameter [6-1:0] OP_LUI   = 6'b001111;
+parameter [6-1:0] OP_JAL   = 6'b000011;
 
 //Main function
 always @ (*) begin
     case (instr_op_i)
-        6'b000000: {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, SinExt_o, ALU_op_o} <= 8'b11001100; // RTYPE
-        6'b000100: {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, SinExt_o, ALU_op_o} <= 8'b00011001; // BEQ
-        6'b001000: {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, SinExt_o, ALU_op_o} <= 8'b10101000; // ADDI
-        6'b001101: {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, SinExt_o, ALU_op_o} <= 8'b10100010; // ORI
-        default:   {RegWrite_o, RegDst_o, ALUSrc_o, Branch_o, SinExt_o, ALU_op_o} <= 8'bxxxxxxxx; // illegal op
+        OP_RTYPE: countrol <= 11'b11000001111; // RTYPE
+        OP_LW   : countrol <= 11'b10100100001; // LW
+        OP_SW   : countrol <= 11'b00101000001; // SW
+        OP_BEQ  : countrol <= 11'b00010000011; // BEQ
+        OP_ADDI : countrol <= 11'b10100000001; // ADDI
+        OP_JUMP : countrol <= 11'b00000010001; // J
+        OP_ORI  : countrol <= 11'b10100000100; // ORI
+        default : countrol <= 11'bxxxxxxxxxxx; // illegal op
     endcase
 end
 
